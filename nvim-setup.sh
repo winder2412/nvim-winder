a3988f7abc086d73262d61f8c213e9df691473f4
winder2412 <winderdawshmaster@gmail.com>
2023-07-11 22:28:16 +0330

    fixed the plugin manager not installing problem

--- nvim-setup.sh
+++ nvim-setup.sh
@@ -1,4 +1,5 @@
 ######this script copy's the nvim directory to your .config directory and install's packer.nvim as the plugin manager#######
 cp -rf ./nvim ~/.config
 ##if you want to use another plugin manager change the script to the install script on your desierd plugin manager##
-./nvim/pluginmanager.sh
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
